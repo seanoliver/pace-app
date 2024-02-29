fc82f9486fa17448dbe684505ede9218c840373d
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2024-02-29 00:50:51 -0800

    wip - initial interval grid layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { DayGrid } from "@/components/day-grid";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>Pace App v0.1</h1>
+      <DayGrid />
     </div>
   );
 }
--- src/components/day-grid.tsx
+++ src/components/day-grid.tsx
@@ -1,7 +1,26 @@
+const HOURS = 24
+const INTERVAL = 15
+
 export const DayGrid = () => {
+
+  const intervalsPerHour = 60 / INTERVAL
+
   return (
     <div>
       <h1>Day Grid</h1>
+      <div>
+        {Array.from({ length: HOURS * intervalsPerHour }).map((_, index) => (
+          <IntervalCell key={index} hour={Math.floor(index / intervalsPerHour)} interval={index % intervalsPerHour} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export const IntervalCell = ({hour, interval}: { hour: number, interval: number }) => {
+  return (
+    <div className="border broder-rose-50 w-10 h-10 flex items-center justify-center">
+      {hour}:{interval}
     </div>
   );
 }
